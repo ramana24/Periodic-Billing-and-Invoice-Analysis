2a7f41364ec8c5dbf2ef84d20e890db120fbf894
ramana24 <yellapuramana@gmail.com>
2024-04-10 12:26:07 +0530

    v1.0 ALP

---
+++ Component-dbg.js
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2009-2021 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+jQuery.sap.declare("isu.in.periodicanalysis1.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("isu.in.periodicanalysis1.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+});
